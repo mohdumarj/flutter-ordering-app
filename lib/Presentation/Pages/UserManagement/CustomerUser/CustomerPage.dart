d42ad1a618d702edec9f04c884fb5afc1fa036c7
Umar <mohd.umarj@gmail.com>
2024-05-05 19:44:17 +0400

    Restaurants data retrieved from firestore and populated on screen Restaurnat details with menu details populated in restaurant details screen

--- lib/Presentation/Pages/UserManagement/CustomerUser/CustomerPage.dart
+++ lib/Presentation/Pages/UserManagement/CustomerUser/CustomerPage.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled9/Entities/RestaurantsModel.dart';
 import 'package:untitled9/Entities/UsersModel.dart';
+import 'package:untitled9/Presentation/Pages/Restaurants/RestaurantDetailScreen.dart';
 
 import '../../../../Globals/Common/Toast.dart';
 import '../../AccountPage.dart';
@@ -22,12 +25,19 @@ class CustomerPage extends StatefulWidget {
 
   final dynamic data;
   CustomerPage({Key? key, required this.data}) : super(key: key);
-
   @override
   State<CustomerPage> createState() => _CustomerPageState();
 }
 
 class _CustomerPageState extends State<CustomerPage> {
+  // Define variables within the state class
+  @override
+  void initState() {
+    super.initState();
+    // Initialization tasks can be performed here
+ //   listedRestaurants = _readRestaurantsData().toList() as List<RestaurantsModel>;
+
+  }
   @override
   Widget build(BuildContext context) {
         return Scaffold(
@@ -38,6 +48,7 @@ class _CustomerPageState extends State<CustomerPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            //Search bar
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
@@ -51,6 +62,7 @@ class _CustomerPageState extends State<CustomerPage> {
               ),
             ),
             SizedBox(height: 20),
+           //Food Type
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
@@ -112,7 +124,8 @@ class _CustomerPageState extends State<CustomerPage> {
                 ),
               ),
             ),
-
+/*
+            //Restaurants label
             SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.all(8.0),
@@ -121,94 +134,92 @@ class _CustomerPageState extends State<CustomerPage> {
                 style: TextStyle(fontSize: 18),
               ),
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  RestaurantLabel(
-                    label: 'Nafahat Burger',
-                    color: Colors.orange,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => NafahatBurgerPage()),
-                      );
-                    },
-                  ),
-                  RestaurantLabel(
-                    label: 'Baith AlShay',
-                    color: Colors.blue,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => BaithAlShaypage()),
-                      );
-                    },
-                  ),
-                  RestaurantLabel(
-                    label: 'Bait AlMadkohout',
-                    color: Colors.green,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => BaitAlMadkohoutPage()),
-                      );
-                    },
-                  ),
-                  RestaurantLabel(
-                    label: '42 Cafe',
-                    color: Colors.purple,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Cafe42Page()),
-                      );
-                    },
-                  ),
-                ],
+            //Restaurants list
+            Row(
+              children: [
+                RestaurantLabel(
+                  label: 'Nafahat Burger',
+                  color: Colors.orange,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NafahatBurgerPage()),
+                    );
+                  },
+                ),
+                RestaurantLabel(
+                  label: 'Baith AlShay',
+                  color: Colors.blue,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => BaithAlShaypage()),
+                    );
+                  },
+                ),
+                RestaurantLabel(
+                  label: 'Bait AlMadkohout',
+                  color: Colors.green,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => BaitAlMadkohoutPage()),
+                    );
+                  },
+                ),
+                RestaurantLabel(
+                  label: '42 Cafe',
+                  color: Colors.purple,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Cafe42Page()),
+                    );
+                  },
+                ),
+              ],
+            ),
+*/
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Restaurants From Data Stream',
+                style: TextStyle(fontSize: 18),
               ),
             ),
-            SizedBox(height: 10),
-      /*      //testCode
+            //Restaurants list Dynamic
             StreamBuilder<List<RestaurantsModel>>(
-                stream: _readRestaurantsData() ,
-                builder: (context, snapshot) {
-                  if(snapshot.connectionState == ConnectionState.waiting){
-                    return Center(child: CircularProgressIndicator(),);
-                  } if(snapshot.data!.isEmpty){
-                    return Center(child:Text("No Data Yet"));
-                  }
-                  final restaurants = snapshot.data;
-                  return Padding(padding: EdgeInsets.all(8),
-                    child: Column(
-                        children: restaurants!.map((restaurant) {
-                          return ListTile(
-                            leading: GestureDetector(
-                              onTap: (){
-                                //_deleteData(user.id!);
-                              },
-                              child: Icon(Icons.delete),
-                            ),
-                            trailing: GestureDetector(
-                              onTap: (){
-                                // _updateData(
-                                //     UserModel(
-                                //       id: user.id,
-                                //       username: "John Wick",
-                                //       adress: "Pakistan",)
-                                // );
-                              },
-                              child: Icon(Icons.update),
-                            ),
-                            title: Text(restaurant.name!),
-                            subtitle: Text(restaurant.address!),
-                          );
-                        }).toList()
-                    ),);
+              stream: _readRestaurantsData(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator(),);
                 }
+                if (snapshot.data!.isEmpty) {
+                  return Center(child: Text("No Data Yet"));
+                }
+                final restaurants = snapshot.data;
+                Iterable<RestaurantLabel> restaurantLabels = restaurants!.map((restaurant) {
+                  return RestaurantLabel(label: restaurant.name!,color: getRandomColor(),onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => RestaurantDetailScreen(restaurant: restaurant)),
+                    );
+                  },);
+                });
+                List<Widget> restaurantWidgets = restaurantLabels.toList();
+                child: return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: restaurantWidgets, // RestaurantLabel widgets as children
+                  ),
+                );
+              }
             ),
-            //testCode
-            */
+            SizedBox(height: 10),
+
 
           ],
         ),
@@ -219,8 +230,8 @@ class _CustomerPageState extends State<CustomerPage> {
           children: [
             IconButton(
               onPressed: () async {
-               RestaurantsModel? restaurant = await getRestaurantDataWithId("20xwFE3GpI6ORDSEabwg") ;
-               print(restaurant!.name);
+              // RestaurantsModel? restaurant = await getRestaurantDataWithId("20xwFE3GpI6ORDSEabwg") ;
+              // print(restaurant!.name);
                 // Navigate to home page
               },
               icon: Icon(Icons.home),
@@ -250,14 +261,14 @@ class _CustomerPageState extends State<CustomerPage> {
       ),
     );
   }
-  /*
+
   Stream<List<RestaurantsModel>> _readRestaurantsData()  {
     try {
       final restaurantCollection = FirebaseFirestore.instance.collection("Restaurants");
 
       var result = restaurantCollection.snapshots().map((qureySnapshot)
       => qureySnapshot.docs.map((e)
-      => RestaurantsModel.fromSnapshot(e),).toList());
+      => RestaurantsModel.fromDocumentSnapshot(e),).toList());
 
       return result;
     } catch (e) {
@@ -266,7 +277,7 @@ class _CustomerPageState extends State<CustomerPage> {
     }
 
   }
-*/
+
   Future<RestaurantsModel?> getRestaurantDataWithId(String restaurantId) async {
     try {
       // Get a reference to the Firestore document using the provided document ID
@@ -289,4 +300,10 @@ class _CustomerPageState extends State<CustomerPage> {
     }
   }
 
+  Color getRandomColor() {
+    final List<Color> colorsList = Colors.primaries; // Get all primary colors
+    final Random random = Random();
+    final int randomIndex = random.nextInt(colorsList.length);
+    return colorsList[randomIndex];
+  }
 }
