c2251eea941ba8ab7cda09c3f1c0bfd17238179a
Umar <mohd.umarj@gmail.com>
2024-05-15 00:57:53 +0400

    added bottom navigation bar to pages

--- lib/Presentation/Pages/CartDetailsWithCheckout/CartDetailsWithCheckoutPage.dart
+++ lib/Presentation/Pages/CartDetailsWithCheckout/CartDetailsWithCheckoutPage.dart
@@ -5,6 +5,7 @@ import '../../../Entities/OrderModel.dart';
 import '../../../Entities/ShoppingCart.dart';
 import '../../../Entities/UserCartItemModel.dart';
 import '../../../Globals/Common/Toast.dart';
+import '../AccountPage.dart';
 import '../CheckOutRow/CheckOutRow.dart';
 import '../UserManagement/CustomerUser/CustomerPage.dart';
 
@@ -42,109 +43,152 @@ class _CartDetailsWithCheckoutPageState
         cart.getItemsTotalPrice(); // Access the singleton instance
     this.items = ShoppingCart().getItems();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Review Order'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Card(
-            elevation: 4, // Adjust the elevation for a shadow effect
-            color: Colors.orangeAccent, // Background color
-            margin: EdgeInsets.all(16), // Adjust the margin as needed
-            child: SizedBox(
-              width: double.infinity,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Customer Name: ${cart.getUserInformation().username}',
-                      style: TextStyle(fontSize: 18),
+        appBar: AppBar(
+          title: Text('Review Order'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              elevation: 4, // Adjust the elevation for a shadow effect
+              color: Colors.orangeAccent, // Background color
+              margin: EdgeInsets.all(16), // Adjust the margin as needed
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Customer Name: ${cart.getUserInformation().username}',
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Email: ${cart.getUserInformation().email}',
-                      style: TextStyle(fontSize: 18),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Email: ${cart.getUserInformation().email}',
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
 
-          // Cart Items
-          Expanded(
-            child: ListView.builder(
-              itemCount: cart.getTotalItemCount(), //.items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return CheckOutRow(
-                  item: item,
-                  onDelete: (deletedItem) {
-                    ShoppingCart().removeItem(deletedItem);
-                    updateCartDetails();
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                            '${deletedItem.menuItem!.name} removed from cart'),
-                        action: SnackBarAction(
-                          label: 'Undo',
-                          onPressed: () {
-                            cart.addItem(deletedItem);
-                            updateCartDetails();
-                          },
+            // Cart Items
+            Expanded(
+              child: ListView.builder(
+                itemCount: cart.getTotalItemCount(), //.items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  return CheckOutRow(
+                    item: item,
+                    onDelete: (deletedItem) {
+                      ShoppingCart().removeItem(deletedItem);
+                      updateCartDetails();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                              '${deletedItem.menuItem!.name} removed from cart'),
+                          action: SnackBarAction(
+                            label: 'Undo',
+                            onPressed: () {
+                              cart.addItem(deletedItem);
+                              updateCartDetails();
+                            },
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                  onIncreaseItem: () {
-                    updateCartDetails();
-                  },
-                  onDecreaseItem: () {
-                    updateCartDetails();
-                  },
-                );
-              },
-            ),
-          ),
-          // Total Price
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Total: \OMR${totalPayable.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          // Checkout Button
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  //call function to place convert data into Order and push it to firebase
-                  placeOrder();
-                  ShoppingCart().clearCart();
-
-                  Navigator.pushReplacement(
-                    context,
-                    //MaterialPageRoute(builder: (context) => RestaurantScreen()),//CustomerPage(data: userProfile?.username ?? 'Customer')),
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            CustomerPage()), //data: userProfile?.username ?? 'Customer')),
+                      );
+                    },
+                    onIncreaseItem: () {
+                      updateCartDetails();
+                    },
+                    onDecreaseItem: () {
+                      updateCartDetails();
+                    },
                   );
                 },
-                child: Text('Finalize and Place Your Order'),
               ),
             ),
+            // Total Price
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Total: \OMR${totalPayable.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            // Checkout Button
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    //call function to place convert data into Order and push it to firebase
+                    placeOrder();
+                    ShoppingCart().clearCart();
+
+                    Navigator.pushReplacement(
+                      context,
+                      //MaterialPageRoute(builder: (context) => RestaurantScreen()),//CustomerPage(data: userProfile?.username ?? 'Customer')),
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              CustomerPage()), //data: userProfile?.username ?? 'Customer')),
+                    );
+                  },
+                  child: Text('Finalize and Place Your Order'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Tooltip(
+                message: 'Home',
+                child: IconButton(
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CustomerPage()),
+                    );
+                  },
+                  icon: Icon(Icons.home),
+                ),
+              ),
+              Tooltip(
+                message: 'Shopping Cart',
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CartDetailsWithCheckoutPage()),
+                    );
+                  },
+                  icon: Icon(Icons.shopping_cart),
+                ),
+              ),
+              Tooltip(
+                message: 'Account',
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AccountPage()),
+                    );
+                  },
+                  icon: Icon(Icons.account_circle),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 
   Future<dynamic> placeOrder() async {
--- lib/Presentation/Pages/Restaurants/RestaurantDetailScreen.dart
+++ lib/Presentation/Pages/Restaurants/RestaurantDetailScreen.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled9/Entities/RestaurantMenuModel.dart';
 import 'package:untitled9/Entities/RestaurantsModel.dart';
 import 'package:untitled9/Entities/ShoppingCart.dart';
 
 import '../../../Entities/UserCartItemModel.dart';
+import '../AccountPage.dart';
 import '../CartDetailsWithCheckout/CartDetailsWithCheckoutPage.dart';
 import '../MenuItemCard.dart';
+import '../UserManagement/CustomerUser/CustomerPage.dart';
 
 class RestaurantDetailScreen extends StatefulWidget {
   final RestaurantsModel restaurant;
@@ -21,111 +22,155 @@ class _RestaurantDetailScreenState extends State<RestaurantDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.restaurant.name ?? "Selected Restaurant"),
-        actions: [
-          Stack(
-            children: [
-              IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {
-                  //CheckoutPage()
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => CartDetailsWithCheckoutPage()),
+        appBar: AppBar(
+          title: Text(widget.restaurant.name ?? "Selected Restaurant"),
+          actions: [
+            Stack(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {
+                    //CheckoutPage()
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CartDetailsWithCheckoutPage()),
+                    );
+                  },
+                ),
+                Positioned(
+                  right: 5,
+                  top: 5,
+                  child: Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 20,
+                      minHeight: 20,
+                    ),
+                    child: Text(
+                      '${ShoppingCart().getTotalItemCount()}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 8,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              elevation: 4, // Adjust the elevation for a shadow effect
+              margin: EdgeInsets.all(16), // Adjust the margin as needed
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Address: ${widget.restaurant.address}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Email: ${widget.restaurant.email}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Phone Number: ${widget.restaurant.phoneNumber}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Website: ${widget.restaurant.website}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.restaurant.menusList.length,
+                itemBuilder: (context, index) {
+                  RestaurantMenuModel menu = widget.restaurant.menusList[index];
+                  return ExpansionTile(
+                    title: Text(menu.menuName ?? "Selected Menu"),
+                    initiallyExpanded: true,
+                    children: menu.menuItems!.map((item) {
+                      UserCartItemModel userCartitem = UserCartItemModel(
+                          resturantId: widget.restaurant.id, menuItem: item);
+                      return MenuItemCard().initMenuItemCard(userCartitem);
+                    }).toList(),
                   );
                 },
               ),
-              Positioned(
-                right: 5,
-                top: 5,
-                child: Container(
-                  padding: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  constraints: BoxConstraints(
-                    minWidth: 20,
-                    minHeight: 20,
-                  ),
-                  child: Text(
-                    '${ShoppingCart().getTotalItemCount()}',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 8,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Tooltip(
+                message: 'Home',
+                child: IconButton(
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CustomerPage()),
+                    );
+                  },
+                  icon: Icon(Icons.home),
+                ),
+              ),
+              Tooltip(
+                message: 'Shopping Cart',
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CartDetailsWithCheckoutPage()),
+                    );
+                  },
+                  icon: Icon(Icons.shopping_cart),
+                ),
+              ),
+              Tooltip(
+                message: 'Account',
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AccountPage()),
+                    );
+                  },
+                  icon: Icon(Icons.account_circle),
                 ),
               ),
             ],
           ),
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Card(
-            elevation: 4, // Adjust the elevation for a shadow effect
-            margin: EdgeInsets.all(16), // Adjust the margin as needed
-            child: SizedBox(
-              width: double.infinity,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Address: ${widget.restaurant.address}',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Email: ${widget.restaurant.email}',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Phone Number: ${widget.restaurant.phoneNumber}',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Website: ${widget.restaurant.website}',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
-              itemCount: widget.restaurant.menusList.length,
-              itemBuilder: (context, index) {
-                RestaurantMenuModel menu = widget.restaurant.menusList[index];
-                return ExpansionTile(
-                  title: Text(menu.menuName ?? "Selected Menu"),
-                  initiallyExpanded: true,
-                  children: menu.menuItems!.map((item) {
-                    UserCartItemModel userCartitem = UserCartItemModel(resturantId: widget.restaurant.id,menuItem: item);
-                    return MenuItemCard().initMenuItemCard(userCartitem);
-                  }).toList(),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
